3cb2be6bc317c16fb22cdf6f7e778dc3449ade09
ahylden <ahylden@yahoo.com>
2023-10-10 00:38:51 -0500

    moved float params

--- diff_drive/src/diff_drive.cpp
+++ diff_drive/src/diff_drive.cpp
@@ -33,9 +33,6 @@ public:
   DiffDrive()
   : Node("diff_drive")
   {
-    float wheel_base = std::stof(declare_parameter<std::string>("wheel_base", ""));
-    float wheel_radius = std::stof(declare_parameter<std::string>("wheel_radius", ""));
-
     subscription_ = this->create_subscription<geometry_msgs::msg::Twist>("cmd_vel", 10, std::bind(&DiffDrive::diff_callback,this, _1));
     publisher_left = this->create_publisher<std_msgs::msg::Float64>("/commands/motor_left/speed", 100);
     publisher_right = this->create_publisher<std_msgs::msg::Float64>("/commands/motor_right/speed", 100);
@@ -60,6 +57,9 @@ private:
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr subscription_;
   rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_left;
   rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr publisher_right;
+
+  float wheel_base = std::stof(declare_parameter<std::string>("wheel_base", ""));
+  float wheel_radius = std::stof(declare_parameter<std::string>("wheel_radius", ""));
 };
 
 int main(int argc, char * argv[])
