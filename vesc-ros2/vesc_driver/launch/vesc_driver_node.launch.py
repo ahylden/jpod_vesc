3567c2379f82834c334b906cf38d03d102ca3299
ahylden <ahylden@yahoo.com>
2023-10-20 23:34:35 -0500

    added lidar to main launch file

--- vesc-ros2/vesc_driver/launch/vesc_driver_node.launch.py
+++ vesc-ros2/vesc_driver/launch/vesc_driver_node.launch.py
@@ -57,7 +57,7 @@ def generate_launch_description():
             name="config_left",
             default_value=vesc_config_left,
             description="VESC yaml configuration file.",
-            ),
+        ),
         Node(
             package='vesc_driver',
             executable='vesc_driver_node',
@@ -68,7 +68,7 @@ def generate_launch_description():
             name="config_right",
             default_value=vesc_config_right,
             description="VESC yaml configuration file.",
-            ),
+        ),
         Node(
             package='vesc_driver',
             executable='vesc_driver_node',
@@ -79,12 +79,34 @@ def generate_launch_description():
             name="config",
             default_value=diff_drive_config,
             description="Differential Drive yaml configuration file.",
-            ),
+        ),
         Node(
             package='diff_drive',
             executable='diff_drive',
             name='diff_drive_node',
             parameters=[LaunchConfiguration("config")]
         ),
+        Node(
+            package='ldlidar_stl_ros2',
+            executable='ldlidar_stl_ros2_node',
+            name='LD19',
+            output='screen',
+            parameters=[
+              {'product_name': 'LDLiDAR_LD19'},
+              {'topic_name': 'scan'},
+              {'frame_id': 'base_laser'},
+              {'port_name': '/dev/ttyUSB0'},
+              {'port_baudrate': 230400},
+              {'laser_scan_dir': True},
+              {'enable_angle_crop_func': False},
+              {'angle_crop_min': 135.0},
+              {'angle_crop_max': 225.0}]
+        ),
+        Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='base_link_to_base_laser_ld19',
+            arguments=['0','0','0.18','0','0','0','base_link','base_laser']
+        )
 
     ])
