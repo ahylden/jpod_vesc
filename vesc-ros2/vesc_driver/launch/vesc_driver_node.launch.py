41c178596042b2ec8442354254bd6e64972e468e
Austin Hylden <ahylden@yahoo.com>
2023-11-18 15:05:40 -0600

    added nav2 and slam to launch

--- vesc-ros2/vesc_driver/launch/vesc_driver_node.launch.py
+++ vesc-ros2/vesc_driver/launch/vesc_driver_node.launch.py
@@ -31,6 +31,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -57,6 +59,23 @@ def generate_launch_description():
         'config',
         'odom_to_tf.yaml'
         )
+    
+    nav2_launch_file = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('nav2_bringup'),
+                'launch/navigation_launch.py')
+            )
+        )
+    
+    slam_toolbox_file = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('slam_toolbox'),
+                'launch/online_async_launch.py')
+            )
+        )
+
     return LaunchDescription([
         DeclareLaunchArgument(
             name="config_left",
@@ -136,5 +155,7 @@ def generate_launch_description():
             executable='odom_to_tf',
             name='odom_to_tf_node',
             parameters=[LaunchConfiguration("config_odom_tf")]
-        )
+        ),
+        nav2_launch_file,
+        slam_toolbox_file
     ])
