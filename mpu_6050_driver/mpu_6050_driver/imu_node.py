f96b2dbf3c3fd3daa7f8d4ca56b22158f77c7d09
Austin Hylden <ahylden@yahoo.com>
2023-11-21 20:26:51 -0600

    rewriting imu py

--- mpu_6050_driver/mpu_6050_driver/imu_node.py
+++ mpu_6050_driver/mpu_6050_driver/imu_node.py
@@ -3,7 +3,7 @@
 import time
 import smbus
 import struct
-import rospy
+import rclpy
 import numpy as np
 from sensor_msgs.msg import Temperature, Imu
 from tf.transformations import quaternion_about_axis
@@ -14,84 +14,101 @@ ADDR = None
 bus = None
 IMU_FRAME = None
 
-# read_word and read_word_2c from http://blog.bitify.co.uk/2013/11/reading-data-from-mpu-6050-on-raspberry.html
-def read_word(adr):
-    high = bus.read_byte_data(ADDR, adr)
-    low = bus.read_byte_data(ADDR, adr+1)
-    val = (high << 8) + low
-    return val
-
-def read_word_2c(adr):
-    val = read_word(adr)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
-        return val
-
-def publish_temp(timer_event):
-    temp_msg = Temperature()
-    temp_msg.header.frame_id = IMU_FRAME
-    temp_msg.temperature = read_word_2c(TEMP_H)/340.0 + 36.53
-    temp_msg.header.stamp = rospy.Time.now()
-    temp_pub.publish(temp_msg)
-
-
-def publish_imu(timer_event):
-    imu_msg = Imu()
-    imu_msg.header.frame_id = IMU_FRAME
-
-    # Read the acceleration vals
-    accel_x = read_word_2c(ACCEL_XOUT_H) / 16384.0
-    accel_y = read_word_2c(ACCEL_YOUT_H) / 16384.0
-    accel_z = read_word_2c(ACCEL_ZOUT_H) / 16384.0
-    
-    # Calculate a quaternion representing the orientation
-    accel = accel_x, accel_y, accel_z
-    ref = np.array([0, 0, 1])
-    acceln = accel / np.linalg.norm(accel)
-    axis = np.cross(acceln, ref)
-    angle = np.arccos(np.dot(acceln, ref))
-    orientation = quaternion_about_axis(angle, axis)
-
-    # Read the gyro vals
-    gyro_x = read_word_2c(GYRO_XOUT_H) / 131.0
-    gyro_y = read_word_2c(GYRO_YOUT_H) / 131.0
-    gyro_z = read_word_2c(GYRO_ZOUT_H) / 131.0
-    
-    # Load up the IMU message
-    o = imu_msg.orientation
-    o.x, o.y, o.z, o.w = orientation
-
-    imu_msg.linear_acceleration.x = accel_x
-    imu_msg.linear_acceleration.y = accel_y
-    imu_msg.linear_acceleration.z = accel_z
-
-    imu_msg.angular_velocity.x = gyro_x
-    imu_msg.angular_velocity.y = gyro_y
-    imu_msg.angular_velocity.z = gyro_z
-
-    imu_msg.header.stamp = rospy.Time.now()
-
-    imu_pub.publish(imu_msg)
-
-
 temp_pub = None
 imu_pub = None
 
-if __name__ == '__main__':
-    rospy.init_node('imu_node')
+class ImuNode(Node):
 
-    bus = smbus.SMBus(rospy.get_param('~bus', 1))
-    ADDR = rospy.get_param('~device_address', 0x68)
+    def __init__(self):
+        super().__init__('imu_node')
+        self.temp_publisher_ = self.create_publisher('temperature', Temperature, 10)
+        self.imu_publisher_ = self.create_publisher('imu/data', Imu, 10)
+
+        imu_timer_period = 0.02  # seconds
+        temp_timer_period = 10  # seconds
+        self.timer = self.create_timer(imu_timer_period, self.publish_imu)
+        self.timer = self.create_timer(temp_timer_period, self.publish_temp)
+    
+    # read_word and read_word_2c from http://blog.bitify.co.uk/2013/11/reading-data-from-mpu-6050-on-raspberry.html
+    def read_word(adr):
+        high = bus.read_byte_data(ADDR, adr)
+        low = bus.read_byte_data(ADDR, adr+1)
+        val = (high << 8) + low
+        return val
+
+    def read_word_2c(adr):
+        val = read_word(adr)
+        if (val >= 0x8000):
+            return -((65535 - val) + 1)
+        else:
+            return val
+
+    def publish_temp(timer_event):
+        temp_msg = Temperature()
+        temp_msg.header.frame_id = IMU_FRAME
+        temp_msg.temperature = read_word_2c(TEMP_H)/340.0 + 36.53
+        temp_msg.header.stamp = rospy.Time.now()
+        temp_pub.publish(temp_msg)
+
+
+    def publish_imu(timer_event):
+        imu_msg = Imu()
+        imu_msg.header.frame_id = IMU_FRAME
+
+        # Read the acceleration vals
+        accel_x = read_word_2c(ACCEL_XOUT_H) / 16384.0
+        accel_y = read_word_2c(ACCEL_YOUT_H) / 16384.0
+        accel_z = read_word_2c(ACCEL_ZOUT_H) / 16384.0
+
+        # Calculate a quaternion representing the orientation
+        accel = accel_x, accel_y, accel_z
+        ref = np.array([0, 0, 1])
+        acceln = accel / np.linalg.norm(accel)
+        axis = np.cross(acceln, ref)
+        angle = np.arccos(np.dot(acceln, ref))
+        orientation = quaternion_about_axis(angle, axis)
+
+        # Read the gyro vals
+        gyro_x = read_word_2c(GYRO_XOUT_H) / 131.0
+        gyro_y = read_word_2c(GYRO_YOUT_H) / 131.0
+        gyro_z = read_word_2c(GYRO_ZOUT_H) / 131.0
+
+        # Load up the IMU message
+        o = imu_msg.orientation
+        o.x, o.y, o.z, o.w = orientation
+
+        imu_msg.linear_acceleration.x = accel_x
+        imu_msg.linear_acceleration.y = accel_y
+        imu_msg.linear_acceleration.z = accel_z
+
+        imu_msg.angular_velocity.x = gyro_x
+        imu_msg.angular_velocity.y = gyro_y
+        imu_msg.angular_velocity.z = gyro_z
+
+        imu_msg.header.stamp = rclpy.Time.now()
+
+        imu_pub.publish(imu_msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    imu_node = ImuNode()
+
+    bus = smbus.SMBus(rclpy.get_parameter('~bus', 1))
+    ADDR = rclpy.get_parameter('~device_address', 0x68)
     if type(ADDR) == str:
         ADDR = int(ADDR, 16)
 
-    IMU_FRAME = rospy.get_param('~imu_frame', 'imu_link')
-
+    IMU_FRAME = rclpy.get_parameter('~imu_frame', 'imu_link')
     bus.write_byte_data(ADDR, PWR_MGMT_1, 0)
 
-    temp_pub = rospy.Publisher('temperature', Temperature, queue_size=10)
-    imu_pub = rospy.Publisher('imu/data', Imu, queue_size=10)
-    imu_timer = rospy.Timer(rospy.Duration(0.02), publish_imu)
-    temp_timer = rospy.Timer(rospy.Duration(10), publish_temp)
-    rospy.spin()
+    rclpy.spin(imu_node)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    imu_node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
