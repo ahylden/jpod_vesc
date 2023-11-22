3cb91558c0d68d58ea6a3d88bb0a672ae23c5d95
Austin Hylden <ahylden@yahoo.com>
2023-11-21 19:23:01 -0600

    adding cfg

--- mpu_6050_driver/setup.py
+++ mpu_6050_driver/setup.py
@@ -2,12 +2,36 @@
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
-d = generate_distutils_setup(
-    packages=['mpu_6050_driver'],
-    scripts=['scripts/imu_node.py'],
-    package_dir={'': 'src'},
-    install_requires=['rospkg'],
-    )
+#d = generate_distutils_setup(
+#    packages=['mpu_6050_driver'],
+#    scripts=['scripts/imu_node.py'],
+#    package_dir={'': 'src'},
+#    install_requires=['rospkg'],
+#    )
 
-setup(**d)
+#setup(**d)
+
+package_name = 'mpu_6050_driver'
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Name',
+    maintainer_email='your@email.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
