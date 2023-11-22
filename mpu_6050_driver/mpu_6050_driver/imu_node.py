e9d9f9257c3043d45e71d392b541a430a090215f
Austin Hylden <ahylden@yahoo.com>
2023-11-22 16:07:11 -0600

    adding self tags

--- mpu_6050_driver/mpu_6050_driver/imu_node.py
+++ mpu_6050_driver/mpu_6050_driver/imu_node.py
@@ -14,66 +14,6 @@ ADDR = None
 bus = None
 IMU_FRAME = None
 
-# read_word and read_word_2c from http://blog.bitify.co.uk/2013/11/reading-data-from-mpu-6050-on-raspberry.html
-def read_word(adr):
-    high = bus.read_byte_data(ADDR, adr)
-    low = bus.read_byte_data(ADDR, adr+1)
-    val = (high << 8) + low
-    return val
-
-def read_word_2c(adr):
-    val = read_word(adr)
-    if (val >= 0x8000):
-        return -((65535 - val) + 1)
-    else:
-        return val
-
-def publish_temp(timer_event, Node):
-    temp_msg = Temperature()
-    temp_msg.header.frame_id = IMU_FRAME
-    temp_msg.temperature = read_word_2c(TEMP_H)/340.0 + 36.53
-    temp_msg.header.stamp = Node.Time.now()
-    temp_pub.publish(temp_msg)
-
-
-def publish_imu(timer_event, Node):
-    imu_msg = Imu()
-    imu_msg.header.frame_id = IMU_FRAME
-
-    # Read the acceleration vals
-    accel_x = read_word_2c(ACCEL_XOUT_H) / 16384.0
-    accel_y = read_word_2c(ACCEL_YOUT_H) / 16384.0
-    accel_z = read_word_2c(ACCEL_ZOUT_H) / 16384.0
-    
-    # Calculate a quaternion representing the orientation
-    accel = accel_x, accel_y, accel_z
-    ref = np.array([0, 0, 1])
-    acceln = accel / np.linalg.norm(accel)
-    axis = np.cross(acceln, ref)
-    angle = np.arccos(np.dot(acceln, ref))
-
-    # Read the gyro vals
-    gyro_x = read_word_2c(GYRO_XOUT_H) / 131.0
-    gyro_y = read_word_2c(GYRO_YOUT_H) / 131.0
-    gyro_z = read_word_2c(GYRO_ZOUT_H) / 131.0
-    
-    # Load up the IMU message
-    o = imu_msg.orientation
-    o.x, o.y, o.z, o.w = 0.0
-
-    imu_msg.linear_acceleration.x = accel_x
-    imu_msg.linear_acceleration.y = accel_y
-    imu_msg.linear_acceleration.z = accel_z
-
-    imu_msg.angular_velocity.x = gyro_x
-    imu_msg.angular_velocity.y = gyro_y
-    imu_msg.angular_velocity.z = gyro_z
-
-    imu_msg.header.stamp = Node.get_clock().now()
-
-    imu_pub.publish(imu_msg)
-
-
 temp_pub = None
 imu_pub = None
 
@@ -88,6 +28,65 @@ class ImuNode(Node):
         temp_timer_period = 10  # seconds
         self.timer = self.create_timer(imu_timer_period, self.publish_imu(self))
         self.timer = self.create_timer(temp_timer_period, self.publish_temp(self))
+    
+    # read_word and read_word_2c from http://blog.bitify.co.uk/2013/11/reading-data-from-mpu-6050-on-raspberry.html
+    def read_word(self, adr):
+        high = bus.read_byte_data(ADDR, adr)
+        low = bus.read_byte_data(ADDR, adr+1)
+        val = (high << 8) + low
+        return val
+
+    def read_word_2c(self, adr):
+        val = self.read_word(adr)
+        if (val >= 0x8000):
+            return -((65535 - val) + 1)
+        else:
+            return val
+
+    def publish_temp(self, timer_event, Node):
+        temp_msg = Temperature()
+        temp_msg.header.frame_id = IMU_FRAME
+        temp_msg.temperature = self.read_word_2c(TEMP_H)/340.0 + 36.53
+        temp_msg.header.stamp = Node.Time.now()
+        temp_pub.publish(temp_msg)
+
+
+    def publish_imu(self, timer_event, Node):
+        imu_msg = Imu()
+        imu_msg.header.frame_id = IMU_FRAME
+
+        # Read the acceleration vals
+        accel_x = self.read_word_2c(ACCEL_XOUT_H) / 16384.0
+        accel_y = self.read_word_2c(ACCEL_YOUT_H) / 16384.0
+        accel_z = self.read_word_2c(ACCEL_ZOUT_H) / 16384.0
+
+        # Calculate a quaternion representing the orientation
+        accel = accel_x, accel_y, accel_z
+        ref = np.array([0, 0, 1])
+        acceln = accel / np.linalg.norm(accel)
+        axis = np.cross(acceln, ref)
+        angle = np.arccos(np.dot(acceln, ref))
+
+        # Read the gyro vals
+        gyro_x = self.read_word_2c(GYRO_XOUT_H) / 131.0
+        gyro_y = self.read_word_2c(GYRO_YOUT_H) / 131.0
+        gyro_z = self.read_word_2c(GYRO_ZOUT_H) / 131.0
+
+        # Load up the IMU message
+        o = imu_msg.orientation
+        o.x, o.y, o.z, o.w = 0.0
+
+        imu_msg.linear_acceleration.x = accel_x
+        imu_msg.linear_acceleration.y = accel_y
+        imu_msg.linear_acceleration.z = accel_z
+
+        imu_msg.angular_velocity.x = gyro_x
+        imu_msg.angular_velocity.y = gyro_y
+        imu_msg.angular_velocity.z = gyro_z
+
+        imu_msg.header.stamp = Node.get_clock().now()
+
+        imu_pub.publish(imu_msg)
 
 def main(args=None):
     rclpy.init(args=args)
